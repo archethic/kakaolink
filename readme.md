e0b67b5c5ae07ea8c75bc7bdbba00d3e244a4c90
naijun0403 <65700898+archethic@users.noreply.github.com>
2021-09-26 21:16:46 +0900

    cookie: fin

--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 # kakaolink
+Test MessengerBotR
+
+채팅 자동응답 봇에서 작동하지 않는다면 ```require("kakaolink/index")```로 해보세요
 ## how to use?
 [example](https://github.com/archethic/kakaolink/blob/main/example/legacy-api.js) is here!
 
---
+++ src/template/components/Button.js
@@ -0,0 +1,13 @@
+/**
+ * Created by naijun on 2021/09/26
+ * Copyright (c) naijun.
+ * This code is licensed under the MIT Licensing Principles.
+ */
+
+module.exports.Button = (function () {
+   function Button() {
+
+   }
+
+   return Button;
+})()
